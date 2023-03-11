1badb5c1ed9e2e9868239f0fa0724f8524e9cf42
Dafter <DafterPlay@mail.ru>
2023-03-12 02:18:52 +0300

    add string check fun

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 // return number from 'start' to 'end'
 const getRadomNumber = (start, end) => Math.round(Math.random() * (end - start) + start);
+// return true if string's length less or equals when 'maxLen'
+const checkStringLen = (str, maxLen) => str.length <= maxLen;
