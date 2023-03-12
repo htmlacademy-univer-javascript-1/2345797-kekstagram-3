a4001359ce2d5b784b425668f9ca67d5cddf90fa
Dafter <DafterPlay@mail.ru>
2023-03-12 04:39:21 +0300

    add tests

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@
 const getRadomNumber = (start, end) => Math.round(Math.random() * (end - start) + start);
 // return true if string's length less or equals when 'maxLen'
 const checkStringLen = (str, maxLen) => str.length <= maxLen;
+console.log(getRadomNumber(0, 10))
+console.log(checkStringLen('aboba', 5))
