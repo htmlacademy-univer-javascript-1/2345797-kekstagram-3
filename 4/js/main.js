26ff540a2aa222a03a8a7b3986c7f1654e610a99
Keks <keks@htmlacademy.ru>
2023-03-16 21:28:49 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+// return number from 'start' to 'end'
+const getRadomNumber = (start, end) => Math.round(Math.random() * (end - start) + start);
+// return true if string's length less or equals when 'maxLen'
+const checkStringLen = (str, maxLen) => str.length <= maxLen;
+getRadomNumber(0, 10);
+checkStringLen('aboba', 5);
